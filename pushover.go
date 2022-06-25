6c59becc00457c3b16c2bdc18a98a2c0b2f25123
Steven Reilly <11709973+TechReilly@users.noreply.github.com>
2022-06-25 17:38:17 +0100

    Rename pushover target environment variable

--- pushover.go
+++ pushover.go
@@ -9,7 +9,7 @@ import (
 // NotifyPushover sends a notification using the Pushover API
 func NotifyPushover(message string) error {
 	token := os.Getenv("PUSHOVER_TOKEN")
-	user := os.Getenv("PUSHOVER_TARGET_USER")
+	user := os.Getenv("PUSHOVER_TARGET")
 	_, err := http.PostForm("https://api.pushover.net/1/messages.json", url.Values{
 		"token":   {token},
 		"user":    {user},
